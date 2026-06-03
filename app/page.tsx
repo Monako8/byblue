2ac6b65c3938f438d5d62a4b020819193a09785c
Monako8 <thekristen3443@gmail.com>
2026-06-03 21:41:47 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,6 @@ const posts: PostData[] = [
     { icon: "/smiles/3.webp", title: "Дизайн", description: "— логотипы, фирменный стиль, упаковка" },
     { icon: "/smiles/1.webp", title: "Web", description: "— лендинги, сайты-портфолио, UI/UX" },
     { icon: "/smiles/4.webp", title: "Концепт-арт", description: "— персонажи для игр, окружение" },
-
     { icon: "/smiles/5.webp", title: "Время работы", description: "— пн-сб: 09:00 - 20:00 (МСК)" },
     { icon: "/smiles/6.webp", title: "Скорость ответа", description: "— в течение 1 часа" },
   ],
